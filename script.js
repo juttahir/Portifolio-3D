c8803350414a78986338cf13c67cc0ae985a2374
Juttahir <juttahir@hotmail.com>
2024-04-14 18:06:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// JavaScript Document
+
+function mostraCranio(){
+			document.getElementById("bg_Imagens").style.display = "block";
+}
+
+function mostraTaca(){
+			document.getElementById("bg_Imagens2").style.display = "block";
+}
+
+function mostraPetrol(){
+			document.getElementById("bg_Imagens3").style.display = "block";
+}
+
+function mostraCasa(){
+			document.getElementById("bg_Imagens4").style.display = "block";
+}
+
+function mostraCerveja(){
+			document.getElementById("bg_Imagens5").style.display = "block";
+}
+
+function mostraChocolate(){
+			document.getElementById("bg_Imagens6").style.display = "block";
+}
+
+function mostraPersona(){
+			document.getElementById("bg_Imagens7").style.display = "block";
+}
+
+function mostraCaneca(){
+			document.getElementById("bg_Imagens8").style.display = "block";
+}
+
+function mostraUmbrella(){
+			document.getElementById("bg_Imagens9").style.display = "block";
+}
+
+function mostraFoguete(){
+			document.getElementById("bg_Imagens10").style.display = "block";
+}
+
+function mostraOnibus(){
+			document.getElementById("bg_Imagens11").style.display = "block";
+}
+
+function mostraF1(){
+			document.getElementById("bg_Imagens12").style.display = "block";
+}
+
+function mostraTocha(){
+			document.getElementById("bg_Imagens13").style.display = "block";
+}
+
+function mostraBeco(){
+			document.getElementById("bg_Imagens14").style.display = "block";
+}
+
+function mostraTrem(){
+			document.getElementById("bg_Imagens15").style.display = "block";
+}
+
+function mostraPS(){
+			document.getElementById("bg_Imagens16").style.display = "block";
+}
+
+function Volta(){
+			document.getElementById("bg_Imagens").style.display = "none";
+}
